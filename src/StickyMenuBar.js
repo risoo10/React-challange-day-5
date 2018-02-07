c7a256a1f79c8e9f0e31f76fb7462bb83cb7607e
risoo10 <polkji10>
2018-02-07 12:49:12 +0100

    Created and styled header app bar / menu.

--- src/StickyMenuBar.js
+++ src/StickyMenuBar.js
@@ -1,11 +1,110 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+import {Menu, Container, Input} from 'semantic-ui-react';
+
+const FullWidthBg = styled.div`
+    background-color: ${props => props.theme.brandBlue};
+    padding: 20px 0px;
+`
+
+const TransparentMenu = styled(Menu)`
+  &&&{
+    background: transparent;
+    border: 0px;
+    box-shadow: none;
+  }
+`
+
+const MenuItem = styled(Menu.Item)`
+  &&&&{
+    color: ${props => props.theme.brandWhite}
+  }    
+`
+
+const MenuItemLink = MenuItem.extend`
+  &&&&&&{
+    text-transform: uppercase;
+    font-size: ${props => props.theme.h3Size};
+    
+    &:before{
+      background: transparent;
+    }
+    
+    &:hover{
+      color: ${props => props.theme.brandBlack};
+      background-color: transparent;
+    }
+  }
+  
+`
+
+const SearchInput = styled(Input)`
+  &&&&&&{
+    background-color: transparent;  
+  }
+  
+  &&&&& > input {
+   background: transparent;
+   color: ${props =>props.theme.brandWhite};
+   border: 2px solid ${props =>props.theme.brandWhite};
+   
+   ::placeholder{
+    color: ${props =>props.theme.brandWhite};
+   }
+  }
+  
+  
+  > i {
+    color: ${props =>props.theme.brandWhite};
+  }
+`
+
+const TransparentMenuMenu = styled(Menu.Menu)`
+  &&&&&{
+    background: transparent;
+    border: 0px;
+    box-shadow: none;
+  }
+`
+
 
 class StickyMenuBar extends Component {
-    render() {
-        return (
-            <div>
 
-            </div>
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: '',
+            menuItems: ["posters", "authors", "topics"]
+        }
+    }
+
+    handleNewItem(name){
+        this.setState({
+            activeItem: name,
+        })
+    }
+
+    render() {
+        const {activeItem, menuItems} = this.state;
+        return (
+            <FullWidthBg>
+                <Container>
+                    <TransparentMenu stackable>
+                        <MenuItem header>Better Quotes</MenuItem>
+                        {menuItems.map(menuItem =>
+                        <MenuItemLink
+                            name={menuItem}
+                            activate={activeItem === menuItem}
+                            onClick={() => this.handleNewItem(menuItem)}
+                        ></MenuItemLink>)}
+                        <TransparentMenuMenu position="right">
+                            <MenuItem>
+                                <SearchInput icon="search" placeholder="Search..."/>
+                            </MenuItem>
+                        </TransparentMenuMenu>
+                    </TransparentMenu>
+                </Container>
+            </FullWidthBg>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import {ThemeProvider} from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = {
+    brandBlue: '#38c9bf',
+    brandWhite: '#ffffff',
+    brandViolet: '#b479c9',
+    brandYellow: '#f4be69',
+    brandBlack: '#41504f',
+    brandGrey: '#bec2c2',
+    redColor: '#f5655d',
+    h1Size: '23px',
+    h2Size: '17px',
+    h3Size: '13px',
+    sectionPadding: '73px 0px',
+}
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}><App /></ThemeProvider>,
+    document.getElementById('root'));
 
