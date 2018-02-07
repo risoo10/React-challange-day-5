c3ce13b71657697689cf0ef6327a5ae4aee49216
risoo10 <polkji10>
2018-02-07 11:13:35 +0100

    Created high level components and setup files.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import TopFiveQuotesPanel from './TopFiveQuotesPanel';
+import StickyMenuBar from './StickyMenuBar';
+import QuotesPanel from './QuotesPanel';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <StickyMenuBar></StickyMenuBar>
+        <TopFiveQuotesPanel></TopFiveQuotesPanel>
+        <QuotesPanel></QuotesPanel>
       </div>
     );
   }
---
+++ src/QuotesPanel.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class QuotesPanel extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default QuotesPanel;
