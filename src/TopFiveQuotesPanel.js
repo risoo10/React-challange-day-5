4531a7c7d06082b7cea4ffffb085e9c4bd3a9be7
risoo10 <polkji10>
2018-02-07 21:48:18 +0100

    Finished top five component with dynamic loading from firebase. Adjusted firebase rules.

--- src/TopFiveQuotesPanel.js
+++ src/TopFiveQuotesPanel.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import {Container} from 'semantic-ui-react';
+import {Container, Grid} from 'semantic-ui-react';
 import {tagsRef, quotesRef} from './firebase';
 import _ from 'lodash';
+
 
 const FullWidthWrapper = styled.div`{
   background: transparent url("top-five-bg.jpg") 0 0;
@@ -29,47 +30,58 @@ const H3 = styled.h3`
   color: ${props => props.theme.brandWhite};
 `
 
+const QuoteWrapper = styled.div`
+  ${props => props.dark && 'background-color: ' + props.theme.brandYellow + ';' };
+  ${props => props.light && 'background-color: ' + props.theme.brandWhite + ';' };
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 370px;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 2em; 
+  box-shadow: -5px 10px 30px rgba(0,0,0,0.3);   
+`
+
+const Quote = styled.p`
+  font-size: 1.5rem; 
+  ${props => props.dark && 'color: ' + props.theme.brandBlack + ';' };
+  ${props => props.light && 'color: ' + props.theme.brandWhite + ';' };    
+`
+
+const Author = styled.p`
+  font-size: 1.1rem; 
+  font-weight: lighter;
+  ${props => props.dark && 'color: ' + props.theme.brandBlack + ';' };
+  ${props => props.light && 'color: ' + props.theme.brandWhite + ';' };    
+`
+
 class TopFiveQuotesPanel extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            quotes: {},
-            tags: {}
+            topQuotes: [],
         }
     }
 
     componentDidMount() {
 
-        let tags = [];
-
-        tagsRef.on('value', snap => {
-            const cloned = _.clone(snap.val());
-            _(cloned).keys().forEach(key => {
-                const newTag = {
-                    key: key,
-                    name: cloned[key],
-                }
-                tags.push(newTag);
-            })
-            this.setState({
-                tags: tags
-            })
-        });
-
-        const quotes = [];
-
-        quotesRef.once('value', snap =>{
+        const topQuotes = [];
+        const popularQuotes = quotesRef.orderByChild('likes').limitToLast(4);
+        popularQuotes.on('value', snap =>{
             const cloned = _.clone(snap.val());
             _(cloned).keys().forEach(key => {
                 const quote = {
                     ...cloned[key],
                     key: key,
                 }
-                quotes.push(quote);
+                topQuotes.push(quote);
             })
             this.setState({
-                quotes: quotes
+                topQuotes: topQuotes
             })
         })
 
@@ -82,6 +94,27 @@ class TopFiveQuotesPanel extends Component {
                     <Container>
                         <H1>Today's popular quotes</H1>
                         <H3>Help us spread the word</H3>
+                        <Grid padded columns={4} doubling container>
+                            <Grid.Row>
+                            {this.state.topQuotes.map( (quote, index) =>
+                                <Grid.Column width={4}>
+                                    <QuoteWrapper
+                                        dark={index % 2 == 1 ? true : false}
+                                        light={index % 2 == 0 ? true : false}
+                                    >
+                                        <Quote
+                                            dark={index % 2 == 0 ? true : false}
+                                            light={index % 2 == 1 ? true : false}
+                                        >"{quote.text}"</Quote>
+                                        <Author
+                                            dark={index % 2 == 0 ? true : false}
+                                            light={index % 2 == 1 ? true : false}
+                                        >- {quote.author}</Author>
+                                    </QuoteWrapper>
+                                </Grid.Column>
+                            )}
+                            </Grid.Row>
+                        </Grid>
                     </Container>
                 </ColorOverlay>
             </FullWidthWrapper>
